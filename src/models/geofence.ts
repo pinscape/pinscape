81741a61d5fbebe86b77e7bb0c7d76428d3d4a3c
Anton Kozlov <kozlovanton.eu@gmail.com>
2017-03-05 06:24:03 +0100

    fix typo

--- src/models/geofence.ts
+++ src/models/geofence.ts
@@ -1,7 +1,7 @@
 export const FIXTURES = [
   { id: "1", latitude: 41.3889, longitude: 2.1135, radius: 10, transitionType: 1, notification: { text: "Level 1: Check the registration docs"} },
   { id: "2", latitude: 41.38911, longitude: 2.11349, radius: 10, transitionType: 1, notification: { text: "Level 2: You received it in exchange for your bag"} },
-  { id: "3", latitude: 50.0671974, longitude: 19.945232, radius: 10, transitionType: 1, notification: { text: Level 3: "xxx"} },
+  { id: "3", latitude: 50.0671974, longitude: 19.945232, radius: 10, transitionType: 1, notification: { text: "Level 3: xxx"} },
   { id: "4", latitude: 52.2287803, longitude: 21.001124, radius: 10, transitionType: 1, notification: { text: "Warsaw Main Station"} },
   { id: "5", latitude: 40.7257093, longitude: -74.0032786, radius: 10, transitionType: 3, notification: { text: "New York - SOHO"} },
   { id: "6", latitude: 34.0033907, longitude: -118.5069657, radius: 10, transitionType: 2, notification: { text: "LA - Santa Monica State Beach"} },

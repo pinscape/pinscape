5828e52c4c00d48ecf04accf6d7b5bfd92caba8a
Anton Kozlov <kozlovanton.eu@gmail.com>
2017-03-05 06:23:24 +0100

    add first two fixtures

--- src/models/geofence.ts
+++ src/models/geofence.ts
@@ -1,9 +1,9 @@
 export const FIXTURES = [
-  { id: "1", latitude: 50.3021079, longitude: 18.6771861, radius: 3000, transitionType: 1, notification: { text: "Gliwice Train Station"} },
-  { id: "2", latitude: 50.4728049, longitude: 19.0736874, radius: 3000, transitionType: 1, notification: { text: "Pyrzowice Airport"} },
-  { id: "3", latitude: 50.0671974, longitude: 19.945232, radius: 3000, transitionType: 1, notification: { text: "Cracow Main Station"} },
-  { id: "4", latitude: 52.2287803, longitude: 21.001124, radius: 3000, transitionType: 1, notification: { text: "Warsaw Main Station"} },
-  { id: "5", latitude: 40.7257093, longitude: -74.0032786, radius: 4000, transitionType: 3, notification: { text: "New York - SOHO"} },
-  { id: "6", latitude: 34.0033907, longitude: -118.5069657, radius: 3000, transitionType: 2, notification: { text: "LA - Santa Monica State Beach"} },
-  { id: "7", latitude: 25.8938595, longitude: -80.1330216, radius: 500, transitionType: 1, notification: { text: "Dexter's Apartment - Miami Bay Harbour" } },
+  { id: "1", latitude: 41.3889, longitude: 2.1135, radius: 10, transitionType: 1, notification: { text: "Level 1: Check the registration docs"} },
+  { id: "2", latitude: 41.38911, longitude: 2.11349, radius: 10, transitionType: 1, notification: { text: "Level 2: You received it in exchange for your bag"} },
+  { id: "3", latitude: 50.0671974, longitude: 19.945232, radius: 10, transitionType: 1, notification: { text: Level 3: "xxx"} },
+  { id: "4", latitude: 52.2287803, longitude: 21.001124, radius: 10, transitionType: 1, notification: { text: "Warsaw Main Station"} },
+  { id: "5", latitude: 40.7257093, longitude: -74.0032786, radius: 10, transitionType: 3, notification: { text: "New York - SOHO"} },
+  { id: "6", latitude: 34.0033907, longitude: -118.5069657, radius: 10, transitionType: 2, notification: { text: "LA - Santa Monica State Beach"} },
+  { id: "7", latitude: 25.8938595, longitude: -80.1330216, radius: 10, transitionType: 1, notification: { text: "Dexter's Apartment - Miami Bay Harbour" } },
 ];
